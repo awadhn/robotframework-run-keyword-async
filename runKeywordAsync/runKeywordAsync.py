bd38bc586aadd4379b52fea9d8443e257949a0d8
awadhn <awadhn@juniper.net>
2017-07-28 11:56:41 +0530

    Moved to Process and disabled logging when inside child process

--- runKeywordAsync/runKeywordAsync.py
+++ runKeywordAsync/runKeywordAsync.py
@@ -7,8 +7,10 @@ class runKeywordAsync:
     def __init__(self):
         self._thread_pool = {}
         self._last_thread_handle = 1
+        self._robot_log_level = BuiltIn().get_variable_value("${LOG_LEVEL}")
 
     def run_method_async(self, keyword, *args, **kwargs):
+        BuiltIn().set_log_level("NONE")
         handle = self._last_thread_handle
         thread = self._threaded_method(keyword, *args, **kwargs)
         thread.start()
@@ -17,6 +19,7 @@ class runKeywordAsync:
         return handle
 
     def run_keyword_async(self, keyword, *args):
+        BuiltIn().set_log_level("NONE")
         handle = self._last_thread_handle
         thread = self._threaded(keyword, *args)
         thread.start()
@@ -32,7 +35,11 @@ class runKeywordAsync:
               result = self._thread_pool[thread].result_queue.get(True, timeout)
               results.append(result)
             except:
-              raise Exception("Thread " + str(thread) + " Failed")
+              BuiltIn().set_log_level(self._robot_log_level)
+              raise Exception("Process " + str(thread) + " Failed")
+        BuiltIn().set_log_level(self._robot_log_level)
+        self._thread_pool = {}
+        self._last_thread_handle = 1
         return results
 
     def get_async_return(self, handle, timeout=60):
@@ -41,36 +48,37 @@ class runKeywordAsync:
             try:
               result = self._thread_pool[handle].result_queue.get(True, timeout)
               del self._thread_pool[handle]
+              BuiltIn().set_log_level(self._robot_log_level)
               return result
             except:
-              raise Exception("Thread " + str(handle) + " Failed")
+              raise Exception("Process " + str(handle) + " Failed")
         else:
-            raise Exception("Passed Thread id " + str(handle) + " is not a valid id")
+            raise Exception("Passed Process id " + str(handle) + " is not a valid id")
 
     def _threaded_method(self, keyword, *args, **kwargs):
         from multiprocessing import Queue
-        from threading import Thread
+        from multiprocessing import Process
 
         def wrapped_f(q, *args, **kwargs):
             ''' Calls the decorated function and puts the result in a queue '''
             ret = BuiltIn().call_method(keyword, *args, **kwargs)
             q.put(ret)
 
-        q = Queue()
-        th = Thread(target=wrapped_f, args=(q,)+args, kwargs=kwargs)
+        q  = Queue()
+        th = Process(target=wrapped_f, args=(q,)+args, kwargs=kwargs)
         th.result_queue = q
         return th
 
     def _threaded(self, keyword, *args):
         from multiprocessing import Queue
-        from threading import Thread
-    
+        from multiprocessing import Process
+
         def wrapped_f(q, *args):
             ''' Calls the decorated function and puts the result in a queue '''
             ret = BuiltIn().run_keyword(keyword, *args)
             q.put(ret)
 
-        q = Queue()
-        th = Thread(target=wrapped_f, args=(q,)+args)
+        q  = Queue()
+        th = Process(target=wrapped_f, args=(q,)+args)
         th.result_queue = q
         return th
