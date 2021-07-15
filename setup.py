bdb38531adda181cc026b8dc9bb89639defaf580
Maxim Aliseiko <maliseiko@de.adit-jv.com>
2021-07-15 11:58:41 +0200

    Remove set_log_level from get_async_keyword to restore functionality

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "robotframework-run-keyword-async",
-    version = "1.0.8",
+    version = "1.0.9",
     description = "Generic Robot Framework library for asynchronous keyword or method execution",
     author = "Awadh Shukla",
     author_email = "shukla.awadh@gmail.com",
